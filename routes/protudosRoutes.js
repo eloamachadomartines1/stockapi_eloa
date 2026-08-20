e53807b54b65eb622c107db96cc0c1e70d5ac0a8
Eloá Machado Martines <eloamachadomartines1@gmail.com>
2026-08-20 10:01:10 -0400

    oi

--- routes/protudosRoutes.js
+++ routes/protudosRoutes.js
@@ -4,9 +4,10 @@ import * as controller from '../controllers/produtosController.js'
 const router = express.Router()
 
 router.post('/produtos', controller.criar);
-router.get('/produtos', controller.listar)
-router.get('/produtos/:id', controller.buscarPorId)
-router.patch('/produtos/:id', controller.atualizar)
+router.get('/produtos', controller.listar);
+router.get('/produtos/:id', controller.buscarPorId);
+router.patch('/produtos/:id', controller.atualizar);
+router.delete('/produtos/:id', controller.deletar);
 
 export default router;
 
